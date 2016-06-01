1058b64de86ac1b201db57c8b01cc4a958a1f8d5
butchbarcelona <butchbarcelona@github.com>
2016-06-01 10:27:03 +0800

    initial commit

---
+++ app/src/main/java/bacon/proj/bacon/services/RestServices.java
@@ -0,0 +1,112 @@
+package bacon.proj.bacon.services;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import bacon.proj.bacon.MainActivity;
+
+/**
+ * Created by tonnyquintos on 11/30/15.
+ */
+public class RestServices {
+
+
+    private static RestServices instance;
+
+    private static String TAG = "test";
+
+    private RestServices(){
+
+    }
+
+    public static RestServices getInstance(){
+        if(instance == null)
+            instance = new RestServices();
+        return instance;
+    }
+
+
+    SharedPreferences sharedpreferences;
+
+    public String callRest(Context ctx, HashMap<String, String> postDataParams) {
+
+        sharedpreferences = ctx.getSharedPreferences(MainActivity.TAG, Context.MODE_PRIVATE);
+
+        String deviceId = sharedpreferences.getString("device_id", "310021001447343338333633");
+        String accessToken = sharedpreferences.getString("access_token", "ed85ceeff03057276a0d00fcd3d32a8fe367a648");
+
+
+        HttpURLConnection conn = null;
+        StringBuilder jsonResults = new StringBuilder();
+        try {
+            String strUrl = "https://api.particle.io/v1/devices/" +deviceId+
+                    "/led?access_token="+accessToken;
+            URL url = new URL(strUrl);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+
+            writer.flush();
+            writer.close();
+            os.close();
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+
+            int read;
+            char[] buff = new char[1024];
+            while ((read = in.read(buff)) != -1) {
+                jsonResults.append(buff, 0, read);
+            }
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "Error processing API URL", e);
+        } catch (IOException e) {
+            Log.e(TAG, "Error connecting to API", e);
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+
+        return jsonResults.toString();
+    }
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException{
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
+    }
+}
